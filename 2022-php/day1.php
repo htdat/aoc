7e3ed3d68bf19e4c6f3394d341b41f2b0fa61f76
Dat Hoang <htdat@users.noreply.github.com>
2022-12-01 13:18:09 +0700

    Day 1

--- 2022-php/day1.php
+++ 2022-php/day1.php
@@ -1,1 +1,17 @@
 <?php
+$input_file = basename( __FILE__, '.php' ) . '.txt';
+$inputs = file( $input_file, FILE_IGNORE_NEW_LINES ); 
+
+$elfs = [];
+$current_elf = null; 
+foreach( $inputs as $calor ) {
+    if ( empty($calor) ) {
+        $current_elf = $current_elf === null ? 0 : $current_elf + 1; 
+    }
+
+    $elfs[$current_elf][] = $calor;
+}
+
+$elfs_total_calor = array_map( 'array_sum' , $elfs );
+
+echo 'Calories that the Elf carrying the most: ' . max( $elfs_total_calor );
