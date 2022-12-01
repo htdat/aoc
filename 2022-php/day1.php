9b02a1a62ebe2d274d6a25226450fd746521915e
Dat Hoang <htdat@users.noreply.github.com>
2022-12-01 13:32:12 +0700

    Day 1 - start 2

--- 2022-php/day1.php
+++ 2022-php/day1.php
@@ -9,9 +9,16 @@ foreach( $inputs as $calor ) {
         $current_elf = $current_elf === null ? 0 : $current_elf + 1; 
     }
 
-    $elfs[$current_elf][] = $calor;
+    $elfs[$current_elf][] = (int) $calor;
 }
 
+// Find max
 $elfs_total_calor = array_map( 'array_sum' , $elfs );
+echo 'Calories that the Elf carrying the most: ' . max( $elfs_total_calor ) . PHP_EOL;
 
-echo 'Calories that the Elf carrying the most: ' . max( $elfs_total_calor );
+// Find top 3 
+$elfs_total_calor_sort = $elfs_total_calor;
+rsort( $elfs_total_calor_sort, SORT_NUMERIC );
+$top_3 = array_slice( $elfs_total_calor_sort, 0, 3, true );
+
+echo 'The top three Elves carry ' . implode( ', ', $top_3 ) . '. Total: ' . array_sum( $top_3 );
