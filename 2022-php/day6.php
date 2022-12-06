8ce0017a5973d122ce7ac993f91e569785c35c0a
Dat Hoang <htdat@users.noreply.github.com>
2022-12-06 12:36:33 +0700

    Day 6 - better approach

--- 2022-php/day6.php
+++ 2022-php/day6.php
@@ -12,7 +12,7 @@ for ( $i = 3; $i < count($chars); $i++ ) {
 }
 
 $res_two = 0;
-for ( $i = 14; $i < count($chars); $i++ ) {
+for ( $i = 13; $i < count($chars); $i++ ) {
 	if ( 14 === count ( array_unique( [
 			$chars[$i-13],
 			$chars[$i-12],
@@ -33,5 +33,25 @@ for ( $i = 14; $i < count($chars); $i++ ) {
 	}
 }
 
+echo 'Hard-work method:' . PHP_EOL;
 echo $res . PHP_EOL; // 1723
-echo $res_two; // 3708
+echo $res_two . PHP_EOL; // 3708
+
+function get_the_first_mark( int $unique_chars, string $signal ): int {
+	$res = 0;
+	$signal_chars = str_split($signal);
+
+	for( $i = $unique_chars; $i <= strlen($signal); $i++ ) {
+		$group = array_slice($signal_chars, $i - $unique_chars, $unique_chars );
+		if ( $unique_chars === count( array_unique($group) ) ) {
+			$res = $i;
+			break;
+		}
+	}
+	return $res;
+}
+
+echo '---------' . PHP_EOL;
+echo 'Generalization: ' . PHP_EOL;
+echo get_the_first_mark( 4, $line ) . PHP_EOL;
+echo get_the_first_mark( 14, $line ) . PHP_EOL;
