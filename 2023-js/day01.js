5b9d8bf8f4a9787d5d18a3c202ad4a67cdf66c0d
Dat Hoang <htdat@users.noreply.github.com>
2023-12-02 13:21:43 +0700

    Day 01 - part 2

--- 2023-js/day01.js
+++ 2023-js/day01.js
@@ -20,3 +20,46 @@ function getArraySum( an_array ) {
 
 const sum = getArraySum(getCals(lines));
 console.log( 'Part 1: ' + sum); // 56108
+
+// Part 2
+const digitMap = {
+    "one": 1,
+    "two": 2,
+    "three": 3,
+    "four": 4,
+    "five": 5,
+    "six": 6,
+    "seven": 7,
+    "eight": 8,
+    "nine": 9
+}
+
+const linesPart2 = lines.map(line => {
+    let firstIndex = {}; 
+    let lastIndex = {};
+    for (const [letter, digit] of Object.entries(digitMap)) {
+        const first = line.indexOf(letter);
+        if ( -1 !== first ) {
+            firstIndex[letter] = first;
+        }
+
+        const last = line.lastIndexOf(letter);
+        if ( -1 !== last ) {
+            lastIndex[letter] = last;
+        }
+    }
+
+    const first = Math.min(...Object.values(firstIndex));
+    const firstLetter = Object.keys(firstIndex).find(key => firstIndex[key] === first);
+
+    const last = Math.max(...Object.values(lastIndex));
+    const lastLetter = Object.keys(lastIndex).find(key => lastIndex[key] === last);
+
+    line = line.replace(firstLetter, digitMap[firstLetter] + '' + firstLetter);
+
+    line = line.slice(0, last+1) + digitMap[lastLetter] + line.slice(last+1);
+    return line;
+});
+
+const sumPart2 = getArraySum(getCals(linesPart2));
+console.log( 'Part 2: ' + sumPart2); // 54983 55603 54976 55652
